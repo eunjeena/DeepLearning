c7b9c4766b266dad0afc440360f1e6bbb0534079
Gina Na <gina@Ginas-MacBook-Air.local>
2020-01-19 16:43:24 -0800

    [VisionNN] Add MNIST DNN classifier scripts and readme file

--- VisionNN/fashion_MNIST.py
+++ VisionNN/fashion_MNIST.py
@@ -37,7 +37,6 @@ model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
 model.compile(optimizer = tf.optimizers.Adam(), #'adam' is good too
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
-
 # callback
 class myCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
@@ -61,6 +60,41 @@ predicted_class = model.predict(test_images) # (10000, 28, 28)
 print('predicted class:', predicted_class.shape) # (10000, 10)
 print("first predicted class is list of 10 probs")
 print(predicted_class[0])
+
+
+print("Using Convolution and Pooling.. Building model again..")
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+# instead of 60000 28x28x1, we need 6000x28x28x1 (single 4D list)
+training_images=training_images.reshape(60000, 28, 28, 1)
+test_images = test_images.reshape(10000, 28, 28, 1)
+
+training_images=training_images / 255.0
+test_images=test_images/255.0
+
+model2 = tf.keras.models.Sequential([
+    # convolution and pooling
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+    # 64: # of convolutions to generate. purely arbitrary, but start with 32
+    # first layer should have input_shape (2x2)=4 -> 1
+  tf.keras.layers.MaxPooling2D(2, 2),
+    # (2 x 2) is quarter the size
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+model2.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
+               metrics=['accuracy'])
+model2.summary()
+model2.fit(training_images, training_labels, epochs=5)
+eval2_ = model2.evaluate(test_images, test_labels)
+# compare
+print("compare between models with convolution/pooling and without..")
+print("loss:", eval_[0], eval2_[0])
+print("accracy:", eval_[1], eval2_[1])
 
 takeaways = '''
 =====================================================================
